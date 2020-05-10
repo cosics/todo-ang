9b149b870a95cee2f9d4691420285688b3ec31bf
Cezara <cezaraluca@yahoo.com>
2020-05-10 19:52:20 +0300

    ih

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'todo-angular';
-}
+export class AppComponent {}
+/*export class AppComponent {
+  title: string = 'ce are cezi de facut';
+
+  constructor() {
+    //console.log(123);
+    //this.title = 'ceva';
+    this.changeTitle('ceva');
+  }
+
+  changeTitle(title: string): void {
+    this.title = title;
+  }
+} */
